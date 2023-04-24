021f0c94c7d3cf3bcbc4b9542d260b941195c626
jaknndiius <72200641+jaknndiius@users.noreply.github.com>
2023-04-24 13:19:24 +0900

    Temporary fix 2

--- javascript/neungin.js
+++ javascript/neungin.js
@@ -1,6 +1,6 @@
 'use strict'
 import {
-  Subject, SubjectList, SuffixType, Setting, Day, loadPage, SelfStudy
+  Subject, SubjectList, SuffixType, Setting, Day, loadPage, SelfStudy, ClassName
 } from 'https://jaknndiius.github.io/TimeTableAPI/timeTableAPI.js';
 // 문학
 const Lit = new SubjectList('문학', ['박수진', '송선용'], { suffixType: SuffixType.ALPABET});
@@ -23,10 +23,10 @@ const Foregin = new Subject('일본어', '유혜인');
 // 창체
 const Creaty = new Subject('창체', '김영준');
 //모의고사
-Setting.addMockTest('2023/03/23');
-Setting.addMockTest('2023/06/01');
-Setting.addMockTest('2023/09/06');
-Setting.addMockTest('2023/11/21');
+Setting.addMoakTest('2023/03/23');
+Setting.addMoakTest('2023/06/01');
+Setting.addMoakTest('2023/09/06');
+Setting.addMoakTest('2023/11/21');
 //시간표 시간
 Setting.setClassTime(ClassName.CLASS1, 8, 0)
 Setting.setClassTime(ClassName.CLASS2, 9, 20)
